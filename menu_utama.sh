d34732b417b7b7f3304484b3ff96607192d322f8
bgropay <bgropay@gmail.com>
2024-07-06 21:09:32 +0700

    Update menu_utama.sh

--- menu_utama.sh
+++ menu_utama.sh
@@ -2,3 +2,9 @@
 # [menu_utama.sh]
 # # file ini merupakan salah satu file dari projek FPR
 
+while true; do
+    echo "******************** Menu utama FPR ********************"
+    echo ""
+    echo "Pilih opsi dari menu:"
+    echo ""
+done
