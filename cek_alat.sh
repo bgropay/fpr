55304a5f034c511ccbe0f7807320d73cee159bce
bgropay <bgropay@gmail.com>
2024-07-06 19:28:03 +0700

    Update cek_alat.sh

--- cek_alat.sh
+++ cek_alat.sh
@@ -5,6 +5,7 @@
 hm="\e[0;32m" # hijau muda
 ht="\e[1;32m" # hijau terang 
 mt="\e[1;31m" # merah terang
+kt="\e[1;33m" # kuning terang 
 bm="\e[0;34m" # biru muda
 r="\e[0m"
 
@@ -42,10 +43,10 @@ echo -e "${bm}Alat penting: memeriksa...${r}"
 for ap in "${dap[@]}"; do
     sleep 0.1
     if ! command -v "${ap}" >/dev/null 2>&1; then
-        echo -e "${r}${ap}...${mt}Error${r}"
+        echo -e "${r}${ap} ... ${mt}Error${r}"
         apbt+=("${ap}")
     else
-        echo -e "${r}${ap}...${ht}OK${r}"
+        echo -e "${r}${ap} ... ${ht}OK${r}"
     fi
 done
 
@@ -56,10 +57,10 @@ echo -e "${bm}Alat opsional: memeriksa...${r}"
 for ao in "${dao[@]}"; do
     sleep 0.1
     if ! command -v "${ao}" >/dev/null 2>&1; then
-        echo -e "${r}${ao}...${mt}Error${r}"
+        echo -e "${r}${ao} ... ${mt}Error${r}"
         aobt+=("${ao}")
     else
-        echo -e "${r}${ao}...${ht}OK${r}"
+        echo -e "${r}${ao} ... ${ht}OK${r}"
     fi
 done
 
@@ -73,7 +74,7 @@ elif [[ "${#aobt[@]}" -ne 0 ]]; then
     exit 1
 else
     echo ""
-    echo "Semua alat yang diperlukan oleh script ini sudah terinstal. Script ini bisa dijalankan."
-    read -p "Tekan [Enter] untuk melanjutkan..."
+    echo -e "${kt}Semua alat yang diperlukan oleh script ini sudah terinstal. Script ini bisa dijalankan.${r}"
+    read -p $'\e[0;32mTekan [Enter] untuk melanjutkan...\e[0m'
     # Tempatkan kode untuk menjalankan program Anda di sini
 fi
