ec26ef570bc0d193222c34d5fe7013eafaa5dbca
bgropay <bgropay@gmail.com>
2024-07-06 16:19:30 +0700

    Update and rename cek_tools.sh to cek_alat.sh

--- cek_alat.sh
+++ cek_alat.sh
@@ -6,10 +6,10 @@ daftar_alat=(
 for alat in "${daftar_alat[@]}"; do
         echo "[*] Mengecek alat ${alat}..."
         if ! command -v "${alat}" >/dev/null 2>&1; then
-                time.sleep(5)
+                sleep 5
                 echo "[-] Alat ${alat} belum terinstal."
         else
-                time.sleep(3)
+                sleep 3
                 echo "[+] Alat ${alat} sudah terinstal."
         fi
 done
