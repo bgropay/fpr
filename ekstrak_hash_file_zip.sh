b662ffdcaa9d9364b158c6bbf5f640e1f2526416
bgropay <bgropay@gmail.com>
2024-07-06 22:45:20 +0700

    Update ekstrak_hash_file_zip.sh

--- ekstrak_hash_file_zip.sh
+++ ekstrak_hash_file_zip.sh
@@ -1,1 +1,37 @@
+#!/bin/bash
+# [ekstrak_hash_file_zip.sh]
+# # file ini merupakan salah satu file dari projek FPR
 
+# variabel warna 
+cm="\e[0;36m" # cyan muda
+ct="\e[1;36m" # cyan terang 
+mt="\e[1;31m" # merah terang
+kt="\e[1;33m" # kuning terang 
+bt="\e[0;34m" # biru terang 
+r="\e[0m"
+
+
+daftar_menu=(
+    "Ekstrak hash file ZIP"
+    "Kembali ke menu utama"
+)
+
+c=1
+
+while true; do
+    clear
+    echo -e "${cm}******************** Menu Ekstrak Hash File ZIP ********************${r}"
+    echo ""
+    echo -e "${ct}Pilih opsi dari menu:${r}"
+    echo -e "${bt}----------${r}"
+    for menu in "${daftar_menu[@]}"; do
+        echo "${c}. ${menu}"
+        if [[ "${c}" -eq 2 ]]; then
+            echo -e "${bt}----------${r}"
+        elif [[ "${c}" -eq 2 ]]; then
+            echo -e "${bt}----------${r}"
+        fi
+        ((c++))
+    done
+    read -p "" ehfz
+done
