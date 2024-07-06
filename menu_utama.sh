c63c43ff7dfec03a2af49497d187f783d6995caa
bgropay <bgropay@gmail.com>
2024-07-06 21:41:57 +0700

    Update menu_utama.sh

--- menu_utama.sh
+++ menu_utama.sh
@@ -2,9 +2,23 @@
 # [menu_utama.sh]
 # # file ini merupakan salah satu file dari projek FPR
 
+daftar_menu=(
+    "Ekstrak hash file ZIP"
+    "Ekstrak hash file RAR"
+    "Ekstrak hash file 7z"
+    "Ekstrak hash file PDF"
+    "Ekstrak hash file Office (docx, xlsx, pptx)"
+    )
+
+c=1
+
 while true; do
     echo "******************** Menu utama FPR ********************"
     echo ""
     echo "Pilih opsi dari menu:"
-    echo ""
+    echo "----------"
+    for menu in "${daftar_menu[@]}"; do
+        echo "${c}. ${menu}"
+        ((c++))
+    done
 done
