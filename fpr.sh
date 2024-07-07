4a9c8dd0e8e1605e50bb077b20b600a72ab60653
bgropay <bgropay@gmail.com>
2024-07-07 06:53:38 +0000

    Update fpr.sh

--- fpr.sh
+++ fpr.sh
@@ -9,22 +9,23 @@ pt="\e[1;37m"
 r="\e[0m"
 
 if [[ $(id -u) -ne 0 ]]; then
-    echo -e "${kt}Script ini harus dijalani sebagai root.${r}"
-    exit 1
+        echo -e "${kt}Script ini harus dijalani sebagai root.${r}"
+        exit 1
 fi
 
 echo -e "${bt}[*] ${pt}Memulai FPR...${r}"
 # menunggu selama 5 detik 
 sleep 5
 
-# file cek_alat.sh
-fca="src/cek_alat.sh"
+# file menu cek_alat.sh
+fmca="src/menu_cek_alat.sh"
 
 # mengecek apakah file cek_alat.sh ada atau tidak 
-if [[ ! -f "${fca}" ]]; then
-    echo -e "${kt}Script ini tidak bisa dijalankan karena file '${fca}' tidak ditemukan.${r}"
-    exit 1
+if [[ ! -f "${fmca}" ]]; then
+        echo ""
+        echo -e "${kt}Script ini tidak bisa dijalankan karena file '${fmca}' tidak ditemukan.${r}"
+        exit 1
 else
-    # menjalankan file cek_alat.sh
-    bash "${fca}"
+        # menjalankan file cek_alat.sh
+        bash "${fmca}"
 fi
