ee6fb61233e25278fe1ab4a769d10eca00ef0166
bgropay <bgropay@gmail.com>
2024-07-07 06:39:29 +0000

    Create menu_ekstrak_hash.sh

---
+++ src/menu_ekstrak_hash.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+# [menu_ekstrak_hash.sh]
+# # file ini merupakan salah satu file dari projek FPR
+
+# variabel warna 
+hm="\e[0;32m" # hijau muda
+ht="\e[1;32m" # hijau terang 
+mt="\e[1;31m" # merah terang
+kt="\e[1;33m" # kuning terang 
+bt="\e[0;34m" # biru terang 
+r="\e[0m"
+
+
+daftar_menu=(
+        "Ekstrak hash file ZIP"
+        "Ekstrak hash file RAR"
+        "Ekstrak hash file 7z"
+        "Ekstrak hash file PDF"
+        "Ekstrak hash file Office (docx, xlsx, pptx)"
+        "Kembali ke menu utama"
+)
+
+c=1
+
+while true; do
+        c=1
+        clear
+        echo -e "${hm}******************** Menu Ekstrak Hash  ********************${r}"
+        echo ""
+        echo -e "${ht}Pilih opsi dari menu:${r}"
+        echo -e "${bt}----------${r}"
+        for menu in "${daftar_menu[@]}"; do
+                echo "${c}. ${menu}"
+                if [[ "${c}" -eq 5 ]]; then
+                        echo -e "${bt}----------${r}"
+                fi
+                ((c++))
+        done
+        # menu ekstrak hash
+        read -p "" meh
+        case "${meh}" in
+                1)
+                        ehfz="src/ekstrak_hash/ekstrak_hash_file_zip.sh"
+                        if [[ ! -f "${ehfz}" ]]; then
+                                echo ""
+                                echo -e "${kt}Script ini tidak bisa dijalankan karena file '${ehfz}' tidak ditemukan.${r}"
+                                exit 1
+                        else
+                                # ekstrak hash file zip
+                                bash "${ehfz}"
+                        fi
+                        ;;
+                2)
+                        # ekstrak hash file rar
+                        ehfr="src/ekstrak_hash/ekstrak_hash_file_rar.sh"
+                        if [[ ! -f "${ehfr}" ]]; then
+                                echo ""
+                                echo -e "${kt}Script ini tidak bisa dijalankan karena file '${ehfr}' tidak ditemukan.${r}"
+                                exit 1
+                        else
+                                bash "${ehfr}"
+                        fi
+                        ;;
+                3)
+                        # ekstrak hash file 7z
+                        ehf7="src/ekstrak_hash/ekstrak_hash_file_7z.sh"
+                        if [[ ! -f "${ehf7}" ]]; then
+                                echo ""
+                                echo -e "${kt}Script ini tidak bisa dijalankan karena file '${ehf7}' tidak ditemukan.${r}"
+                                exit 1
+                        else
+                                bash "${ehf7}"
+                        fi
+                        ;;
+                4)
+                        # ekstrak hash file pdf
+                        ehfp="src/ekstrak_hash/ekstrak_hash_file_pdf.sh"
+                        if [[ ! -f "${ehfp}" ]]; then
+                                echo ""
+                                echo -e "${kt}Script ini tidak bisa dijalankan karena file '${ehfp}' tidak ditemukan.${r}"
+                                exit 1
+                        else
+                                bash "${ehfp}"
+                        fi
+                        ;;
+                5)
+                        # ekstrak hash file office 
+                        ehfo="src/ekstrak_hash/ekstrak_hash_file_office.sh"
+                        if [[ ! -f "${ehfo}" ]]; then
+                                echo""
+                                echo -e "${kt}Script ini tidak bisa dijalankan karena file '${ehfo}' tidak ditemukan.${r}"
+                                exit 1
+                        else
+                                bash "${ehfo}"
+                        fi
+                        ;;
+        esac
+done
