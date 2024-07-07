5dfad3c4cf56c443bf41ab9f210c6fc479bd87f7
bgropay <bgropay@gmail.com>
2024-07-07 06:22:14 +0000

    Create menu_buat_daftar_kata_sandi.sh

---
+++ src/menu_buat_daftar_kata_sandi.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# [menu_buat_daftar_kata_sandi.sh]
+# # file ini merupakan salah satu file dari projek FPR
+
+# variabel warna 
+hm="\e[0;32m" # hijau muda
+ht="\e[1;32m" # hijau terang 
+mt="\e[1;31m" # merah terang
+kt="\e[1;33m" # kuning terang 
+bt="\e[0;34m" # biru terang 
+r="\e[0m"
+
+
+daftar_menu=(
+        "Buat daftar kata sandi (Wordlist) dengan cupp"
+        "Buat daftar kata sandi (Wordlist) dengan bopscrk"
+        "Kembali ke menu utama"
+)
+
+c=1
+
+while true; do
+        c=1
+        clear
+        echo -e "${hm}******************** Menu Membuat Daftar Kata Sandi (Wordlist)  ********************${r}"
+        echo ""
+        echo -e "${ht}Pilih opsi dari menu:${r}"
+        echo -e "${bt}----------${r}"
+        for menu in "${daftar_menu[@]}"; do
+                echo "${c}. ${menu}"
+                if [[ "${c}" -eq 2 ]]; then
+                        echo -e "${bt}----------${r}"
+                elif [[ "${c}" -eq 3 ]]; then
+                        echo -e "${bt}----------${r}"
+                fi
+                ((c++))
+        done
+        # menu buat wordlist
+        read -p "" mbw
+        case "${mbw}" in
+                1)
+                        # membuat daftar kata sandi dengan cupp
+                        cupp -i
+                        break
+                ;;
+                2)
+                        # membuat daftar kata sandi dengan bopscrk
+                        bopscrk -i
+                        break
+                ;;
+        esac
+done
