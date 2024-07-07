54d0298ec7fa0a78107c87aae9554e31d9bd977f
bgropay <bgropay@gmail.com>
2024-07-07 20:12:43 +0700

    Update menu_ekstrak_hash_file.sh

--- src/menu_ekstrak_hash_file.sh
+++ src/menu_ekstrak_hash_file.sh
@@ -222,8 +222,16 @@ while true; do
                         break
                         ;;
                 6)
-                        bash src/menu_utama.sh
-                        break
+                        fmu="src/menu_utama.sh"
+                        if [[ ! -f "${fmu}" ]]; then
+                                echo ""
+                                echo -e "${kt}Script ini tidak bisa dijalankan karena file '${fmu}' tidak ditemukan.${r}"
+                                read -p "Tekan [Enter] untuk melanjutkan..."
+                                exit 1
+                        else
+                                bash "${fmu}"
+                                break
+                        fi
                         ;;
                 *)
                         echo ""
