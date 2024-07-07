25ff0eb9489874d9ad60736a425b79c04aad2275
bgropay <bgropay@gmail.com>
2024-07-07 20:15:00 +0700

    Update menu_pulihkan_kata_sandi_file.sh

--- src/menu_pulihkan_kata_sandi_file.sh
+++ src/menu_pulihkan_kata_sandi_file.sh
@@ -207,6 +207,17 @@ while true; do
                         read -p "Tekan [Enter] untuk melanjutkan..."
                         break
                         ;;
+                6)
+                        fmu="src/menu_utama.sh"
+                        if [[ ! -f "${fmu}" ]]; then
+                                echo ""
+                                echo -e "${kt}Script ini tidak bisa dijalankan karena file '${fmu}' tidak ditemukan.${r}"
+                                read -p "Tekan [Enter] untuk melanjutkan..."
+                                exit 1
+                        else
+                                bash "${fmu}"
+                                break
+                        fi
                 *)
                         echo ""
                         echo -e "${kt}Menu '${mpksf}' tidak tersedia. Silahkan pilih menu yang tersedia.${r}"
