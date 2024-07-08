68d9bf5317834ca19b148b0104c044fbcf3bd632
bgropay <bgropay@gmail.com>
2024-07-08 21:33:44 +0700

    Create banner.py

---
+++ src/lainnya/banner.py
@@ -0,0 +1,23 @@
+# [ banner.py ]
+
+from colorama import Fore
+
+h = Fore.LIGHTBLACK_EX
+
+print(f"""{h}
+           _
+      /b_,dM\__,_
+   _/MMMMMMMMMMMm,
+  _YMMMMMMMMMMMM(
+  `MMMMMM/   /   \    
+   MMM|  __  / __/
+   YMM/_/# \__/# \   
+   (.   \__/  \__/   
+    )       _,  |  
+_____/\     _   / 
+    \  `._____,'
+  `..___(__
+            ``-.
+                \
+                 )
+""")
