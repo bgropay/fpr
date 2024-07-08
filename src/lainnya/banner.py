c2f66f1ce3ce573e95a450905b81b32ffbfa9bd8
bgropay <bgropay@gmail.com>
2024-07-08 22:26:14 +0700

    Update banner.py

--- src/lainnya/banner.py
+++ src/lainnya/banner.py
@@ -7,25 +7,37 @@ m = Fore.LIGHTRED_EX     # Warna Merah
 r = Fore.RESET           # Reset Warna 
 
 print(f"""
-
-                    .
-                  .-`---/ -
-              \ \``    `/`~,__
-               /        \   \             +------------------------------------------------+
-               |  ./` _/    |             |  Disclaimer                                    |
-              `\   (`     -- \            +------------------------------------------------+
-               //   _      __)            |  Script ini dibuat hanya untuk tujuan edukasi. |
-          __,--`     \   o / _   /~~~\    |  Tidak ada niatan untuk mendorong melakukan    |
-         /    `|_    /\___/  \\_/   //    |  kegiatan ilegal.                              |
-       _/        `~~`   \     \   __/     +------------------------------------------------+
-      /  /               |    /  /         
-    _/ \/     bgropay |_/`\  /  /        
-   <  / |             | \  \/  /         
-    \  \|             |  \    /          
-     \  \__           _\  \__/           
-      \   \\__________/`\
-       \___/           ~~\
-       |                  \              
-       |/       \        \/
- ______|        |'       /_______________________________________________
+{h}-------------------------------------------------------------------------------
+{h};::::.. . . .    ..oedMMVMMMMbeo.    .   .     .        {m}Script ini dibuat hanya 
+{h}:::.. . . .  ..!!!!eMMMMVMMMNWWUUX.    .    .     .
+{h}:.. . .  . -!<- .edMMMMMIMMMMMMMMMMMo    .     .     .   . {m}untuk tujuan edukasi 
+{h}: . .  ,~   ~::WMMMMMMMMIMMMMMMMMMMMVMu     .     .               .         .
+{h}. .  .      ::dUUUUMUMMM!MMMMMMMMMMMVMMM:      .     .       .
+{h}.   '     .::dUUUUUUUUUM!MMMMMMMMMMVVMMMR   .     .      .     {m}dan pembelajaran 
+{h}.  '      .::UUUUUUUUUUMUMMMMMMMMMVVVVMVVk     .     .
+{h}  '      . ':UU''''''ZZZMMMMPP'''''VVZMVV%..      .          .            {m}saja.
+{h} :    :. !: dP:'     'ZZZRMX::     `RVVTMF ::::,..   .   .        .
+{h} '  :!!!:~!!E!:      sZZBBM9::      dVVVM' ::::;`::                   {m}Tidak ada
+{h} ':: `.!!!!!N!..     @WEMEM@k:.     MVVVV``::::! ;:  .       .
+{h} !!!!!!(`~~`Mkb.   :@UUR%%MMMu:.   d9VVV* _`::!!         .        .
+{h} !!!!!!!!>  'Teeoe@@UUU ! 9MMMMWeee@M**P.'  :!!!  .  .         {m}niat atau maksud 
+{h} `!!!!!!!      '^##UUU' ! `MR*TTT#TT       !!!!'             .
+{h}  !!!!!!         ->.., .   .  .. _
+{h}  '      ` .   ,B*'.e$$B$$$$6@Bc 'N,     `     {m}untuk mendorong kegiatan ilegal. 
+{h}           .  :$f d$$$$$$$$$$$$$c $N..    ;
+{h}           := `$>'$$F `#$$$' '$$$ 9$6.^'= ;
+{h}            += *L $$   '$$E   3$$ 'I`''-,
+{h}            `  '4.$$,  J$$N   @$EzE '' ...
+{h}           -*%Ne$@$$b.z$$$$k.z$$$Ph .WP'73&
+{h}          td'  ''$$$*' ..LB$$$$$$Ned#`: ^'B'
+{h}          #$    '$$F 4B$$Lz/*B$$$$$$BNeunuF
+{h}            *=* 3$$6e. '^ .y$@$$'#$$W`'''
+{h}                '$$$$$$$$$$P*'   z$$$r
+{h}                 '$$$$C''^    .z$$$*B
+{h}                   5N$@$$$$W$@$$N$$B'
+{h}                    '$$$$$$$$$$$$$*' .
+{h}                    , ^*$$$$$$$$#      .
+{h}                 ,+'    ''#'''.z6
+{h}             ,+ '      J*#b$@$*''        `+
+{h}         ,+'          J$$eu...              `  .
 """)
