255ec571108d984e083326491f6c6df72903c70f
bgropay <bgropay@gmail.com>
2024-07-08 21:51:56 +0700

    Update banner.py

--- src/lainnya/banner.py
+++ src/lainnya/banner.py
@@ -2,26 +2,44 @@
 
 from colorama import Fore
 
-c = Fore.LIGHTCYAN_EX   # Warna Cyan
-m = Fore.LIGHTRED_EX    # Warna Merah 
-k = Fore.LIGHTYELLOW_EX # Warna Kuning 
-h = Fore.LIGHTBLACK_EX  # Warna Hitam
-r = Fore.RESET          # Reset Warna 
+c = Fore.LIGHTGREE _EX   # Warna Hijau 
+m = Fore.LIGHTRED_EX     # Warna Merah 
+k = Fore.LIGHTYELLOW_EX  # Warna Kuning 
+h = Fore.LIGHTBLACK_EX   # Warna Hitam
+r = Fore.RESET           # Reset Warna 
 
 print(f"""{h}
-{c}           _
-{c}      /b_,dM\__,_
-{c}   _/MMMMMMMMMMMm,
-{c}  _YMMMMMMMMMMMM(
-{c}  `MMMMMMMMMMMMMM\    
-{c}   MMMMMMMMMMMMMM/
-{c}   YMM/{h}_/# \__/# \.{r}   
-{k}   (.   {h}\__/  \__/{r}   
-{k}    )       _,  |  
-{k}_____/\     _   / 
-{k}    \  `._____,'
-{k}  `..___(__
-{k}            ``-.
-{k}                \.
-{k}                 )
+-------------------------------------------------------------------------------
+;::::.. . . .    ..oedMMVMMMMbeo.    .   .     .  Script ini hanya untuk tujuan 
+:::.. . . .  ..!!!!eMMMMVMMMNWWUUX.    .    .     .
+:.. . .  . -!<- .edMMMMMIMMMMMMMMMMMo    .     .     .   .   .   .   .  edukasi   
+: . .  ,~   ~::WMMMMMMMMIMMMMMMMMMMMVMu     .     .               .       .
+. .  .      ::dUUUUMUMMM!MMMMMMMMMMMVMMM:      .     .       .
+.   '     .::dUUUUUUUUUM!MMMMMMMMMMVVMMMR   .     .      .         .     .  dan
+.  '      .::UUUUUUUUUUMUMMMMMMMMMVVVVMVVk     .     .
+  '      . ':UU""""""ZZZMMMMPP"""""VVZMVV%..      .          .     pembelajaran 
+ :    :. !: dP:'     'ZZZRMX::     `RVVTMF ::::,..   .   .        .
+ '  :!!!:~!!E!:      sZZBBM9::      dVVVM" ::::;`::                       saja!
+ ':: `.!!!!!N!..     @WEMEM@k:.     MVVVV``::::! ;:  .       .
+ !!!!!!(`~~`Mkb.   :@UUR%%MMMu:.   d9VVV* _`::!!         .        .
+ !!!!!!!!>  'Teeoe@@UUU ! 9MMMMWeee@M**P.'  :!!!  .  .              
+ `!!!!!!!      '^##UUU' ! `MR*TTT#TT       !!!!'             .
+  !!!!!!         ->.., .   .  .. _
+  '      ` .   ,B*".e$$B$$$$6@Bc "N,     `      `
+           .  :$f d$$$$$$$$$$$$$c $N..    ;
+           := `$>'$$F `#$$$" '$$$ 9$6.^"= ;      https://github.com/bgropay/fpr
+            += *L $$   '$$E   3$$ 'I`""*-,
+            `  '4.$$,  J$$N   @$EzE "" ...
+           -*%Ne$@$$b.z$$$$k.z$$$Ph .WP"73&
+          td"  '"$$$*" ..LB$$$$$$Ned#`: ^"B'
+          #$    '$$F 4B$$Lz/*B$$$$$$BNeunuF
+            *=* 3$$6e. "^ .y$@$$"#$$W`"""
+                '$$$$$$$$$$P*"   z$$$r
+                 "$$$$C""^    .z$$$*B
+                   5N$@$$$$W$@$$N$$B'
+                    "$$$$$$$$$$$$$*' .
+                    , ^*$$$$$$$$#      .
+                 ,+'    '""#""".z6
+             ,+ '      J*#b$@$*""        `+
+         ,+'          J$$eu...              `  .
 """)
