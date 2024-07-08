25aaf708994ec0b1b8b450d93ee49546cc07e53a
bgropay <bgropay@gmail.com>
2024-07-08 23:36:48 +0700

    Update banner.sh

--- src/banner.sh
+++ src/banner.sh
@@ -5,8 +5,10 @@ k="\e[1;33m"
 p="\e[1;37m"
 r="\e[0m"
 
+clear
+
 echo -e "+----------------------------------------+"
-echo -e "| Peringatan. Script ini dibuat hanya    |"
+echo -e "| ${k}Peringatan: ${p}Script ini dibuat hanya    |${r}"
 echo -e "| untuk tujuan edukasi dan pembelajaran  |"
 echo -e "| saja. Tidak ada niat atau maksud untuk |"
 echo -e "| mendorong kegiatan ilegal.             |"
