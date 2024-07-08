96a7212ee0de989a4e692431090ab1da6bf82d58
bgropay <bgropay@gmail.com>
2024-07-08 23:24:36 +0700

    Create banner.sh

---
+++ src/banner.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# [ banner.sh ]
+
+
+
+h="\e[1;32m" # Warna Hijau 
+m="\e[1;31m" # Warna Merah 
+r="\e[0m "   # Reset Warna 
+
+clear
+
+echo "${h}-------------------------------------------------------------------------------${r}"
+echo "{h};::::.. . . .    ..oedMMVMMMMbeo.    .   .     .        ${m}Script ini dibuat hanya${r}" 
+echo "{h}:::.. . . .  ..!!!!eMMMMVMMMNWWUUX.    .    .     .${r}"
+echo "{h}:.. . .  . -!<- .edMMMMMIMMMMMMMMMMMo    .     .     .   . ${m}untuk tujuan edukasi ${r}"
+echo "{h}: . .  ,~   ~::WMMMMMMMMIMMMMMMMMMMMVMu     .     .               .         .${r}"
+echo "{h}. .  .      ::dUUUUMUMMM!MMMMMMMMMMMVMMM:      .     .       .${r}"
+echo "{h}.   '     .::dUUUUUUUUUM!MMMMMMMMMMVVMMMR   .     .      .     ${m}dan pembelajaran ${r}"
+echo "{h}.  '      .::UUUUUUUUUUMUMMMMMMMMMVVVVMVVk     .     .${r}"
+echo "{h}  '      . ':UU''''''ZZZMMMMPP'''''VVZMVV%..      .          .            ${m}saja.${r}"
+echo "{h} :    :. !: dP:'     'ZZZRMX::     `RVVTMF ::::,..   .   .        .${r}"
+echo "{h} '  :!!!:~!!E!:      sZZBBM9::      dVVVM' ::::;`::                   ${m}Tidak ada${r}"
+echo "{h} ':: `.!!!!!N!..     @WEMEM@k:.     MVVVV``::::! ;:  .       .${r}"
+echo "{h} !!!!!!(`~~`Mkb.   :@UUR%%MMMu:.   d9VVV* _`::!!         .        .${r}"
+echo "{h} !!!!!!!!>  'Teeoe@@UUU ! 9MMMMWeee@M**P.'  :!!!  .  .         ${m}niat atau maksud ${r}"
+echo "{h} `!!!!!!!      '^##UUU' ! `MR*TTT#TT       !!!!'             .${r}"
+echo "{h}  !!!!!!         ->.., .   .  .. _{r}"
+echo "{h}  '      ` .   ,B*'.e$$B$$$$6@Bc 'N,     `     ${m}untuk mendorong kegiatan ilegal. ${r}"
+echo "{h}           .  :$f d$$$$$$$$$$$$$c $N..    ;${r}"
+echo "{h}           := `$>'$$F `#$$$' '$$$ 9$6.^'= ;${r}"
+echo "{h}            += *L $$   '$$E   3$$ 'I`''-,${r}"
+echo "{h}            `  '4.$$,  J$$N   @$EzE '' ...${r}"
+echo "{h}           -*%Ne$@$$b.z$$$$k.z$$$Ph .WP'73&${r}"
+echo "{h}          td'  ''$$$*' ..LB$$$$$$Ned#`: ^'B'${r}"
+echo "{h}          #$    '$$F 4B$$Lz/*B$$$$$$BNeunuF${r}"
+echo "{h}            *=* 3$$6e. '^ .y$@$$'#$$W`'''${r}"
+echo "{h}                '$$$$$$$$$$P*'   z$$$r${r}"
+echo "{h}                 '$$$$C''^    .z$$$*B${r}"
+echo "{h}                   5N$@$$$$W$@$$N$$B'${r}"
+echo "{h}                    '$$$$$$$$$$$$$*' .${r}"
+echo "{h}                    , ^*$$$$$$$$#      .${r}"
+echo "{h}                 ,+'    ''#'''.z6${r}"
+echo "{h}             ,+ '      J*#b$@$*''        `+${r}"
+echo "{h}         ,+'          J$$eu...              `  .${r}"
+
+read -p "Tekan [Enter] untuk melanjutkan..."
