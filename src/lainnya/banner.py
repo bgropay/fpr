9a5886d729a1a0a8e6268be7e83f0c95e93997ed
bgropay <bgropay@gmail.com>
2024-07-08 21:38:06 +0700

    Update banner.py

--- src/lainnya/banner.py
+++ src/lainnya/banner.py
@@ -2,22 +2,25 @@
 
 from colorama import Fore
 
-h = Fore.LIGHTBLACK_EX
+c = Fore.LIGHTCYAN_EX  # Warna Cyan
+m = Fore.LIGHTRED_EX   # Warna Merah 
+h = Fore.LIGHTBLACK_EX # Warna Hitam
+r = Fore.RESET         # Reset Warna 
 
 print(f"""{h}
-           _
-      /b_,dM\__,_
-   _/MMMMMMMMMMMm,
-  _YMMMMMMMMMMMM(
-  `MMMMMM/   /   \    
-   MMM|  __  / __/
-   YMM/_/# \__/# \   
-   (.   \__/  \__/   
-    )       _,  |  
-_____/\     _   / 
-    \  `._____,'
-  `..___(__
-            ``-.
-                \
-                 )
+{c}           _
+{c}      /b_,dM\__,_
+{c}   _/MMMMMMMMMMMm,
+{c}  _YMMMMMMMMMMMM(
+{c}  `MMMMMM/   /   \    
+{c}   MMM|  __  / __/
+{c}   YMM/_/# \__/# \   
+{k}   (.   \__/  \__/   
+{k}    )       _,  |  
+{k}_____/\     _   / 
+{k}    \  `._____,'
+{k}  `..___(__
+{k}            ``-.
+{k}                \
+{k}                 )
 """)
