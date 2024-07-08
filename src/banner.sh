ca3313daa8c1548eccdd4730c563f7a607434350
bgropay <bgropay@gmail.com>
2024-07-08 23:50:00 +0700

    Update banner.sh

--- src/banner.sh
+++ src/banner.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 # [ banner.sh ]
 
-k="\e[1;33m"
-p="\e[1;37m"
+c="\e[1;36m" # cyan
+k="\e[1;33m" # kuning
+l="\e[0;33m" # coklat 
+p="\e[1;37m" # putih 
 r="\e[0m"
 
 clear
 
-echo -e "+----------------------------------------+"
-echo -e "| ${k}Peringatan: ${p}Script ini dibuat hanya    |${r}"
-echo -e "| untuk tujuan edukasi dan pembelajaran  |"
-echo -e "| saja. Tidak ada niat atau maksud untuk |"
-echo -e "| mendorong kegiatan ilegal.             |"
-echo -e "+----------------------------------------+"
-echo -e "        \   ^__^"
-echo -e "         \  (oo)\_______"
-echo -e "            (__)\       )\/\."
-echo -e "                ||----w |"
-echo -e "                ||     ||"
-echo -e ""
+echo -e "${c}+----------------------------------------+${r}"
+echo -e "${c}| ${k}Peringatan: ${p}Script ini dibuat hanya    |${r}"
+echo -e "${c}| ${p}untuk tujuan edukasi dan pembelajaran  |${r}"
+echo -e "${c}| ${p}saja. Tidak ada niat atau maksud untuk |${r}"
+echo -e "${c}| ${p}mendorong kegiatan ilegal.             |${r}"
+echo -e "${c}+----------------------------------------+${r}"
+echo -e "${c}        \   ${l}^__^                          ${r}"
+echo -e "${c}         \  ${l}(oo)\_______                  ${r}"
+echo -e "${l}            (__)\       )\/\.                 ${r}"
+echo -e "${l}                ||----w |                     ${r}"
+echo -e "${l}                ||     ||                     ${r}"
+echo -e "${r}"
 
 read -p "Tekan [Enter] untuk melanjutkan..."
