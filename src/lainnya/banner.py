d45dbb0e50c496d2e67475180c6a18b926a9b637
bgropay <bgropay@gmail.com>
2024-07-08 22:12:15 +0700

    Update banner.py

--- src/lainnya/banner.py
+++ src/lainnya/banner.py
@@ -7,37 +7,25 @@ m = Fore.LIGHTRED_EX     # Warna Merah
 r = Fore.RESET           # Reset Warna 
 
 print(f"""
-{h}-------------------------------------------------------------------------------{r}
-{h};::::.. . . .    ..oedMMVMMMMbeo.    .   .     .  {m}Script ini hanya untuk tujuan{r} 
-{h}:::.. . . .  ..!!!!eMMMMVMMMNWWUUX.    .    .     .{r}
-{h}:.. . .  . -!<- .edMMMMMIMMMMMMMMMMMo    .     .     .   .   .   .   .  {m}edukasi{r}   
-{h}: . .  ,~   ~::WMMMMMMMMIMMMMMMMMMMMVMu     .     .               .       .{r}
-{h}. .  .      ::dUUUUMUMMM!MMMMMMMMMMMVMMM:      .     .       .{r}
-{h}.   '     .::dUUUUUUUUUM!MMMMMMMMMMVVMMMR   .     .      .         .     .  {m}dan{r}
-{h}.  '      .::UUUUUUUUUUMUMMMMMMMMMVVVVMVVk     .     .{r}
-{h}  '      . ':UU'''''ZZZMMMMPP'''''VVZMVV%..      .          .      {m}pembelajaran{r} 
-{h} :    :. !: dP:'     'ZZZRMX::     `RVVTMF ::::,..   .   .        .{r}
-{h} '  :!!!:~!!E!:      sZZBBM9::      dVVVM" ::::;`::                       {m}saja!{r}
-{h} ':: `.!!!!!N!..     @WEMEM@k:.     MVVVV``::::! ;:  .       .{r}
-{h} !!!!!!(`~~`Mkb.   :@UUR%%MMMu:.   d9VVV* _`::!!         .        .{r}
-{h} !!!!!!!!>  'Teeoe@@UUU ! 9MMMMWeee@M**P.'  :!!!  .  .              {r}
-{h} `!!!!!!!      '^##UUU' ! `MR*TTT#TT       !!!!'             .{r}
-{h}  !!!!!!         ->.., .   .  .. _{r}
-{h}  '      ` .   ,B*'.e$$B$$$$6@Bc 'N,     `      `{r}
-{h}              .  :$f d$$$$$$$$$$$$$c $N..    ;{r}
-{h}           := `$>'$$F `#$$$' '$$$ 9$6.^'= ;      {m}https://github.com/bgropay/fpr{r}
-{h}            += *L $$   '$$E   3$$ 'I`''*-,{r}
-{h}            `  '4.$$,  J$$N   @$EzE '' ...{r}
-{h}           -*%Ne$@$$b.z$$$$k.z$$$Ph .WP'73&{r}
-{h}          td'  ''$$$*' ..LB$$$$$$Ned#`: ^'B'{r}
-{h}          #$    '$$F 4B$$Lz/*B$$$$$$BNeunuF{r}
-{h}            *=* 3$$6e. '^ .y$@$$'#$$W`'''{r}
-{h}                '$$$$$$$$$$P*'   z$$$r{r}
-{h}                 '$$$$C''^    .z$$$*B{r}
-{h}                   5N$@$$$$W$@$$N$$B'{r}
-{h}                    "$$$$$$$$$$$$$*' .{r}
-{h}                    , ^*$$$$$$$$#      .{r}
-{h}                 ,+'    '''#'''.z6{r}
-{h}             ,+ '      J*#b$@$*''        `+{r}
-{h}         ,+'          J$$eu...              `  .{r}
+
+                    .
+                  .-`---/ -
+              \ \``    `/`~,__
+               /        \   \             +------------------------------------------------+
+               |  ./` _/    |             |  Disclaimer                                    |
+              `\   (`     -- \            +------------------------------------------------+
+               //   _      __)            |  Script ini dibuat hanya untuk tujuan edukasi. |
+          __,--`     \   o / _   /~~~\    |  Tidak ada niatan untuk mendorong melakukan    |
+         /    `|_    /\___/  \\_/   //    |  kegiatan ilegal.                              |
+       _/        `~~`   \     \   __/     +------------------------------------------------+
+      /  /               |    /  /         
+    _/ \/     bgropay |_/`\  /  /        
+   <  / |             | \  \/  /         
+    \  \|             |  \    /          
+     \  \__           _\  \__/           
+      \   \\__________/`\
+       \___/           ~~\
+       |                  \              
+       |/       \        \/
+ ______|        |'       /_______________________________________________
 """)
