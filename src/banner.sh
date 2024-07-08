9191f3bf205ca8acf1bb2319a657241e4303b897
bgropay <bgropay@gmail.com>
2024-07-08 23:29:35 +0700

    Update banner.sh

--- src/banner.sh
+++ src/banner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # [ banner.sh ]
 
-t="Peringatan. Script ini dibuat hanya untuk tujuan edukasi dan pembelajaran saja. Tidak ada niat atau maksud untuk mendorong kegiatan ilegal."
+t="\e[1;33mPeringatan. \e[1;37mScript ini dibuat hanya untuk tujuan edukasi dan pembelajaran saja. Tidak ada niat atau maksud untuk mendorong kegiatan ilegal.\e[0m"
 
 cowsay "${t}"
