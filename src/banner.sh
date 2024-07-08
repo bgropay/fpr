f6f8359002526d3481fa6a02e08ee7a3846e0d03
bgropay <bgropay@gmail.com>
2024-07-08 23:30:11 +0700

    Update banner.sh

--- src/banner.sh
+++ src/banner.sh
@@ -3,4 +3,6 @@
 
 t="\e[1;33mPeringatan. \e[1;37mScript ini dibuat hanya untuk tujuan edukasi dan pembelajaran saja. Tidak ada niat atau maksud untuk mendorong kegiatan ilegal.\e[0m"
 
-cowsay "${t}"
+c=$(cowsay "${t}")
+
+echo -e "${c}"
