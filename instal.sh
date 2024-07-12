9f3d87b06d8f1d17ebb41a904cea0b15fdd4fbed
bgropay <bgropay@gmail.com>
2024-07-12 19:24:18 +0700

    Update instal.sh

--- instal.sh
+++ instal.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 # [instal.sh]
 
+# path atau lokasi folder untuk menyimpan tools 
+path="/opt"
 
+# url john the ripper 
+url_john="https://www.openwall.com/john/k/john-1.9.0-jumbo-1.tar.gz"
+# url hashcat
+url_hashcat="https://hashcat.net/files/hashcat-6.2.6.tar.gz"
 
-# install john the ripper
+# dependensi yang diperlukan oleh john the ripper 
 dependensi_john=(
     "libcrypt1"
     "libcrypt1-dev"
@@ -27,9 +33,21 @@ dependensi_john=(
     "python3-pip"
 )
 
+# dependensi yang diperlukan oleh hashcat
+dependensi_hashcat=(
+    "build-essential"
+    "libssl-dev"
+    "zlib1g-dev"
+)
+
+echo "[*] Menginstal seluruh dependensi yang diperlukan oleh John The Ripper."
+sleep 3
 for instal_dependensi_john in "${dependensi_john[@]}"; do
     apt-get install "${instal_dependensi_john}" -y
 done
+
+echo "[*] Menginstal seluruh dependensi yang diperlukan oleh Hashcat."
+sleep 3
 
 cd /usr/share
 cd JohnTheRipper
